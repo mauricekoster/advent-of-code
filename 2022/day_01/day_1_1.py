c12fbaf44e61b84de3b2293919c44c46d466d8f1
Maurice Koster <maurice@mauricekoster.com>
2022-12-01 12:31:55 +0100

    Cleanup

--- 2022/day_01/day_1_1.py
+++ 2022/day_01/day_1_1.py
@@ -3,10 +3,9 @@ from pprint import pprint
 with open("input.txt", "r") as f:
     content = f.read()
 
-
 elfs = []
 for elf in content.split('\n\n'):
     calories = [int(x) for x in elf.split('\n')]
     elfs.append(sum(calories))
 
-pprint(max(elfs)    )
+pprint(max(elfs))
