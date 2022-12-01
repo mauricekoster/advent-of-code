88298f3f4040dacd271b6d2ee91b566afa97d308
Maurice Koster <maurice@mauricekoster.com>
2022-12-01 12:56:10 +0100

    Make it more unreadable :D

--- 2022/day_01/day_1_1.py
+++ 2022/day_01/day_1_1.py
@@ -3,9 +3,4 @@ from pprint import pprint
 with open("input.txt", "r") as f:
     content = f.read()
 
-elfs = []
-for elf in content.split('\n\n'):
-    calories = [int(x) for x in elf.split('\n')]
-    elfs.append(sum(calories))
-
-pprint(max(elfs))
+pprint(max([sum(cal) for cal in [map(int, elf.split('\n')) for elf in content.split('\n\n')]]))
